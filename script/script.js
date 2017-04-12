5bd464d6df94572903083ad0205eda1f17aa5527
harish-rzt <harish@Harish-V.local>
2017-04-12 12:11:50 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#hdr-nav-btn").click(function(){
+        $(".rr-header__nav").fadeToggle();
+    });
+    $(".rr-header__nav__link").click(function(){
+        $(".rr-header__nav").fadeToggle();
+    })
+});
