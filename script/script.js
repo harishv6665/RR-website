f7f44bee4f7a6049475c3b5e187db6d3d38133fc
harish-rzt <harishv6665@gmail.com>
2017-07-14 19:12:13 +0530

    responsive

--- script/script.js
+++ script/script.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
     $(".rr-header__nav__link").click(function(){
         $(".rr-header__nav").fadeToggle();
     })
+
+    $('#js-ad-close-btn').click(function(){
+        $('#js-ad-popup').hide();
+    })	
 });
